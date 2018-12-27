822089d00dffa2275c7d78566c937bdc64f34ec1
Blake Niemyjski <bniemyjski@gmail.com>
2018-12-27 13:07:17 -0600

    exclude azure storage emulator from running in release mode.

--- tests/Foundatio.AzureStorage.Tests/AzureStorageEmulatorCollection.cs
+++ tests/Foundatio.AzureStorage.Tests/AzureStorageEmulatorCollection.cs
@@ -2,6 +2,8 @@
 using Xunit;
 
 namespace Foundato.Azure.Tests {
+#if DEBUG
     [CollectionDefinition("AzureStorageIntegrationTests")]
     public class AzureStorageEmulatorCollection : ICollectionFixture<AzureStorageEmulatorFixture> { }
+#endif
 }
