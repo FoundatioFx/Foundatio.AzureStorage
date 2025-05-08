4f86b309e09f518f89aaf015dc999952078fe00e
Blake Niemyjski <bniemyjski@gmail.com>
2025-05-08 10:14:30 -0500

    Updated deps

--- src/Foundatio.AzureStorage/Queues/AzureStorageQueue.cs
+++ src/Foundatio.AzureStorage/Queues/AzureStorageQueue.cs
@@ -146,7 +146,7 @@ public class AzureStorageQueue<T> : QueueBase<T, AzureStorageQueueOptions<T>> wh
 
     public override async Task AbandonAsync(IQueueEntry<T> entry)
     {
-        _logger.LogDebug("Queue {QueueName}:{QueueId} abandon item: {QueueEntryId}", _options.Name, QueueId, entry.Id);
+        _logger.LogDebug("Queue {QueueName} ({QueueId}) abandon item: {QueueEntryId}", _options.Name, QueueId, entry.Id);
         if (entry.IsAbandoned || entry.IsCompleted)
             throw new InvalidOperationException("Queue entry has already been completed or abandoned.");
 
